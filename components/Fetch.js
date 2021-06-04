df13b22d96b6ef76a0b6e0a848c1dca8cf22d77d
Sasa Gvozdenovic <sasa.gvozdenovic@shareit.rs>
2021-06-04 15:14:40 +0200

    Done

---
+++ components/Fetch.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react'
+
+const Fetch = () => {
+    const [data, setData] = useState()
+    const photosPrefix = 'https://dimedia-nekretnine.nekretninebrcko.com/thumb.php?file='
+
+    useEffect(() => {
+        fetch('http://localhost:3000/api/hello')
+            .then(response => response.json())
+            .then(data => setData(data.realEstates.realEstate));
+    }, [])
+
+    console.log(data)
+    //console.log(data.re_photo[0].re_photos_name._cdata)
+    var x = data?.re_photo[0]?.re_photos_name?._cdata;
+    return (
+        <>
+            <p>{data?.re_realEstates_code?._cdata}</p>
+            <img style={{ width: '300px' }} src={photosPrefix + x} />
+        </>
+    )
+}
+
+export default Fetch
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,28 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
+
+  var convert = require('xml-js');
+  var axios = require('axios');
+
+  axios
+    .get('https://dimedia-nekretnine.nekretninebrcko.com/synchronization_data/web/xml.xml', {
+      headers: {
+        'Content-Type': "application/xml; charset=utf-8",
+      },
+      mode: 'no-cors',
+    })
+    .then(function (response) {
+      var data = response.data;
+      var result1 = convert.xml2json(data, { compact: true, spaces: 4 });
+      //var result2 = convert.xml2json(data, { compact: false, spaces: 4 });
+      res.status(200).json(JSON.parse(result1))
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+
+
+
 }
