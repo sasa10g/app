c083789e1515a1a30079a0b63d9faf8d5d27e203
Sasa <sasagvozdenovic@hotmail.com>
2021-06-08 01:06:34 +0200

    img url fixed

--- components/Fetch.js
+++ components/Fetch.js
@@ -36,7 +36,7 @@ const Fetch = () => {
 
   console.log(data);
   //console.log(data.re_photo[0].re_photos_name._cdata)
-  var x = data?.re_photo[0]?.re_photos_name?._cdata;
+  var x = data?.realEstates?.realEstate?.re_photo[0]?.re_photos_name?._cdata;
   return (
     <>
       <p>{data?.realEstates?.realEstate?.re_realEstates_code?._cdata}</p>
